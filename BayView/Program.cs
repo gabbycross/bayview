ebc54cfe8cb3a6207b0deb8d74dd6ab5f9f01a12
Jamie <Jamie@LAPTOP-JJNB5VDG>
2017-11-04 12:53:41 +0000

    DB Connect class added

--- BayView/Program.cs
+++ BayView/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace BayView
 {
-    static class Program
+    class Program
     {
         /// <summary>
         /// The main entry point for the application.
---
+++ BayView/dbConnect.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BayView
+{
+    class dbConnect
+    {
+       
+        public static string
+          source = @"Data Source=C:\Users\Jamie\Desktop\Uni Work\Team App project\BayView.db";
+
+    }
+}
